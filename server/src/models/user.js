bf8d9dfde8f0b503860c5f9a65f2f24795d49504
Manish karki <karkimanish188@gmail.com>
2024-07-17 18:18:27 +0545

    adding schema

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,9 +2,30 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose;
 
 const chatScheme = new Schema({
-    name: String
+    email: String,
+    username:String,
+    sendername: String,
+    receivername:String,
+    message : String,
+    gender:{
+        type: String,
+        enum:['Male','Female','other'],
+        default :'Male'
+    },
+    role:{
+      type:String,
+      enum:['sender',"receiver"],
+      default:'sender'
+    },
+    password:String,
+   
+
 });
 
 
 const User = mongoose.model('User', chatScheme);
 module.exports = User
+
+
+
+//
